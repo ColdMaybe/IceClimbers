b5e9e38e1abfdab149f7f557b5ce819a7718f42b
Cold <89932386+ColdMaybe@users.noreply.github.com>
2022-05-20 21:33:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
-let intro = document.querySelector('.intro');
-let logo = document.querySelector('.logo-header');
-let logoSpan = document.querySelectorAll('.logo');
 
-window.addEventListener('DOMContentLoaded', ()=>{
-    
-    setTimeout(()=>{
-        logoSpan.forEach((span, idx)=>{
-            setTimeout(()=>{
-                span.classList.add('active');
-            }, (idx + 1) * 400)
-        });
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
 
-        setTimeout(() => {
-            logoSpan.forEach((span, idx) => {
 
-                setTimeout(() => {
-                    span.classList.remove('active');
-                    span.classList.add('fade'); 
-                }, (idx + 1) * 50)
-            })
-
-        }, 2000);
-
-        setTimeout(() => {
-            intro.style.top = '-100vh';
-        }, 2300);
-    
-    
-    })
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
 })
+
+
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+
+document.addEventListener('mousemove', move);
+function move(e){
+    this.querySelectorAll('.move').forEach(layer =>{
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX*speed)/120
+        const y = (window.innerHeight - e.pageY*speed)/120
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
+
+
+gsap.from('.nav_logo, .nav_toggle', {opacity: 0, duration: 1, delay:2, y: 10})
+gsap.from('.nav_item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
+
+gsap.from('.home_title', {opacity: 0, duration: 1, delay:1.6, y: 30})
+gsap.from('.home_description', {opacity: 0, duration: 1, delay:1.8, y: 30})
+gsap.from('.home_button', {opacity: 0, duration: 1, delay:2.1, y: 30})
+gsap.from('.home_img', {opacity: 0, duration: 1, delay:1.3, y: 30})
